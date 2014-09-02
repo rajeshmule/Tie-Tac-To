3c79719dbe52959ffbc4daa598e41755f5cb3ad4
rajeshmule <rajeshsmule@gmail.com>
2014-09-02 17:54:51 +0530

    add splash screen

--- src/com/example/game/MainActivity.java
+++ src/com/example/game/MainActivity.java
@@ -17,13 +17,10 @@ public class MainActivity extends ActionBarActivity {
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+		this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+				WindowManager.LayoutParams.FLAG_FULLSCREEN);
 		requestWindowFeature(Window.FEATURE_NO_TITLE);
 		setContentView(R.layout.activity_main);
-		
-		
-		
-		
 
 		web = (WebView) findViewById(R.id.webView);
 
